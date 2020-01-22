04a8f78e87adfdb9a54926326a5e00ca865c7e9a
v_vjdhuang <v_vjdhuang@tencent.com>
2020-01-22 11:37:46 +0800

    clear cached

---
+++ jdutil/build/generated/source/buildConfig/debug/cn/ddh/jdutil/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package cn.ddh.jdutil;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "cn.ddh.jdutil";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "cn.ddh.jdutil";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "V1.0.0";
+}
